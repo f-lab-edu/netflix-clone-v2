23bb825dc0b20c4616dd53e17008c2226449a53c
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2024-12-26 21:26:00 +0900

    add: svg loader

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,17 @@ import i18nConfig from './next-i18next.config'
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  i18n: i18nConfig.i18n
+  i18n: i18nConfig.i18n,
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      }
+    }
+  }
 };
 
 export default nextConfig;

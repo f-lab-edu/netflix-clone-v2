0b96ec354a27fc926bdc84ea643207f06807bc64
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2024-12-30 16:58:59 +0900

    fix: direction as proxy type

--- src/components/styled/layout.ts
+++ src/components/styled/layout.ts
@@ -61,60 +61,57 @@ export const position = {
   })
 }
 
-export const direction = {
-  all0: css({
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0
-  }),
-  x0: css({
-    left: 0,
-    right: 0
-  }),
-  y0: css({
-    top: 0,
-    bottom: 0,
-  }),
-  t0: css({
-    top: 0,
-  }),
-  b0: css({
-    bottom: 0,
-  }),
-  l0: css({
-    left: 0
-  }),
-  r0: css({
-    right: 0
-  }),
-  all1: css({
-    top: '1rem',
-    bottom: '1rem',
-    left: '1rem',
-    right: '1rem',
-  }),
-  x1: css({
-    left: '1rem',
-    right: '1rem',
-  }),
-  y1: css({
-    top: '1rem',
-    bottom: '1rem',
-  }),
-  t1: css({
-    top: '1rem',
-  }),
-  b1: css({
-    bottom: '1rem',
-  }),
-  l1: css({
-    left: '1rem',
-  }),
-  r1: css({
-    right: '1rem',
-  })
-}
+const directionObj: {
+  [k: `all${number}`]: SerializedStyles | SerializedStyles[]
+  [k: `x${number}`]: SerializedStyles | SerializedStyles[]
+  [k: `y${number}`]: SerializedStyles | SerializedStyles[]
+  [k: `t${number}`]: SerializedStyles | SerializedStyles[]
+  [k: `b${number}`]: SerializedStyles | SerializedStyles[]
+  [k: `l${number}`]: SerializedStyles | SerializedStyles[]
+  [k: `r${number}`]: SerializedStyles | SerializedStyles[]
+} = {}
+export const direction = new Proxy<typeof directionObj>(directionObj, {
+  get(target, k) {
+    if (!/(all|x|y|t|b|l|r)([0-9.]+)/.test(String(k))) return undefined
+    const key = k.toString() as keyof typeof directionObj
+    if (!target[key]) {
+      const strK = String(key)
+      const size = strK.split(/(all|x|y|t|b|l|r)/)
+      const num = Number(size[2])
+      const tempList = []
+      switch (size[1]) {
+        case 'all':
+          tempList.push(direction[`x${num}`])
+          tempList.push(direction[`y${num}`])
+          break
+        case 'x':
+          tempList.push(direction[`l${num}`])
+          tempList.push(direction[`r${num}`])
+          break
+        case 'y':
+          tempList.push(direction[`t${num}`])
+          tempList.push(direction[`b${num}`])
+          break
+        case 'l':
+          target[`l${num}`] = css({ left: num && `${num}rem` })
+          break
+        case 'r':
+          target[`r${num}`] = css({ right: num && `${num}rem` })
+          break
+        case 't':
+          target[`t${num}`] = css({ top: num && `${num}rem` })
+          break
+        case 'b':
+          target[`b${num}`] = css({ bottom: num && `${num}rem` })
+          break
+      }
+      if (tempList.length) {
+        target[key] = tempList.flat()
+      }
+    }
+    return target[key]
+  }
+})
 
 export const zIndex = {
   bg: css({

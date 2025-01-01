5d57fc7ea9ff13bec8b025b27e8b180bd2d49e8a
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-01-01 14:46:27 +0900

    add: direction helper

---
+++ src/components/styled/helper/DirectionHelper.ts
@@ -0,0 +1,109 @@
+import type { CSSObject, SerializedStyles } from '@emotion/react';
+import { css } from '@emotion/react';
+
+export type SizeObjectType = {
+  [k: `a-${number | string}`]: SerializedStyles | SerializedStyles[]
+  [k: `w-${number | string}`]: SerializedStyles | SerializedStyles[]
+  [k: `h-${number | string}`]: SerializedStyles | SerializedStyles[]
+}
+
+export type DirectionObjectType = {
+  [k: `a-${number | string}`]: SerializedStyles | SerializedStyles[]
+  [k: `x-${number | string}`]: SerializedStyles | SerializedStyles[]
+  [k: `y-${number | string}`]: SerializedStyles | SerializedStyles[]
+  [k: `t-${number | string}`]: SerializedStyles | SerializedStyles[]
+  [k: `b-${number | string}`]: SerializedStyles | SerializedStyles[]
+  [k: `l-${number | string}`]: SerializedStyles | SerializedStyles[]
+  [k: `r-${number | string}`]: SerializedStyles | SerializedStyles[]
+}
+
+const directionPattern = /(a|x|y|t|b|l|r)-([a-z0-9.%]+|[0-9.]+)/
+const sizePattern = /(a|w|h)-([a-z0-9.%]+|[0-9.]+)/
+
+export const generateSizeObject = (
+  obj: SizeObjectType,
+  width: keyof CSSObject,
+  height: keyof CSSObject
+) => {
+  const proxyObj = new Proxy<SizeObjectType>(obj, {
+    get(target, k) {
+      if (!sizePattern.test(String(k))) return undefined
+      const key = k.toString() as keyof typeof obj
+      if (!target[key]) {
+        const strK = String(key)
+        const size = strK.split(sizePattern)
+        const num = Number(size[2])
+        const tempList = []
+        switch (size[1]) {
+          case 'a':
+            tempList.push(proxyObj[`w-${size[2]}`])
+            tempList.push(proxyObj[`h-${size[2]}`])
+            break
+          case 'w':
+            target[key] = css({ [width]: Number.isNaN(num) ? size[2] : num && `${num}rem` })
+            break
+          case 'h':
+            target[key] = css({ [height]: Number.isNaN(num) ? size[2] : num && `${num}rem` })
+            break
+        }
+        if (tempList.length) {
+          target[key] = tempList.flat()
+        }
+      }
+      return target[key]
+    }
+  })
+  return proxyObj
+}
+
+export const generateDirectionObject = (
+  obj: DirectionObjectType,
+  left: keyof CSSObject,
+  right: keyof CSSObject,
+  top: keyof CSSObject,
+  bottom: keyof CSSObject
+) => {
+  const proxyObj = new Proxy<DirectionObjectType>(obj, {
+    get(target, k) {
+      if (!directionPattern.test(String(k))) return undefined
+      const key = k.toString() as keyof typeof obj
+      if (!target[key]) {
+        const strK = String(key)
+        const size = strK.split(directionPattern)
+        const num = Number(size[2])
+        const tempList = []
+        switch (size[1]) {
+          case 'a':
+            tempList.push(proxyObj[`x-${size[2]}`])
+            tempList.push(proxyObj[`y-${size[2]}`])
+            break
+          case 'x':
+            tempList.push(proxyObj[`l-${size[2]}`])
+            tempList.push(proxyObj[`r-${size[2]}`])
+            break
+          case 'y':
+            tempList.push(proxyObj[`t-${size[2]}`])
+            tempList.push(proxyObj[`b-${size[2]}`])
+            break
+          case 'l':
+            target[key] = css({ [left]: Number.isNaN(num) ? size[2] : num && `${num}rem` })
+            break
+          case 'r':
+            target[key] = css({ [right]: Number.isNaN(num) ? size[2] : num && `${num}rem` })
+            break
+          case 't':
+            target[key] = css({ [top]: Number.isNaN(num) ? size[2] : num && `${num}rem` })
+            break
+          case 'b':
+            target[key] = css({ [bottom]: Number.isNaN(num) ? size[2] : num && `${num}rem` })
+            break
+        }
+        if (tempList.length) {
+          target[key] = tempList.flat()
+        }
+      }
+      return target[key]
+    }
+  })
+  return proxyObj
+}
--- src/components/styled/layout.ts
+++ src/components/styled/layout.ts
@@ -1,6 +1,8 @@
+import type { DirectionObjectType, SizeObjectType } from './helper/DirectionHelper';
 import type { CSSObject, SerializedStyles } from '@emotion/react';
 import { css } from '@emotion/react'
 import facepaint from 'facepaint'
+import { generateDirectionObject, generateSizeObject } from './helper/DirectionHelper';
 
 type DisplaySize = 'xs' | 'sm' | 'md' | 'lg' | 'full'
 
@@ -27,73 +29,36 @@ export const BreakPoints = {
   full: (args: CSSObject | SerializedStyles | (CSSObject | SerializedStyles)[]) => css({ [`@media (min-width: ${sizes.full}px)`]: args })
 }
 
-export const w = {
-  full: css({
-    width: '100%'
-  })
-}
+const sizeObj: SizeObjectType = {}
+export const size = generateSizeObject(
+  sizeObj,
+  'width',
+  'height'
+)
 
-export const h = {
-  full: css({
-    height: '100%'
-  })
-}
+const directionObj: DirectionObjectType = {}
+export const direction = generateDirectionObject(
+  directionObj,
+  'left',
+  'right',
+  'top',
+  'bottom'
+)
 
-export const wh = {
-  full: css([
-    w.full,
-    h.full
-  ])
-}
+const marginObject: DirectionObjectType = {}
+export const margin = generateDirectionObject(
+  marginObject,
+  'marginLeft',
+  'marginRight',
+  'marginTop',
+  'marginBottom'
+)
 
-const directionObj: {
-  [k: `all${number}`]: SerializedStyles | SerializedStyles[]
-  [k: `x${number}`]: SerializedStyles | SerializedStyles[]
-  [k: `y${number}`]: SerializedStyles | SerializedStyles[]
-  [k: `t${number}`]: SerializedStyles | SerializedStyles[]
-  [k: `b${number}`]: SerializedStyles | SerializedStyles[]
-  [k: `l${number}`]: SerializedStyles | SerializedStyles[]
-  [k: `r${number}`]: SerializedStyles | SerializedStyles[]
-} = {}
-export const direction = new Proxy<typeof directionObj>(directionObj, {
-  get(target, k) {
-    if (!/(all|x|y|t|b|l|r)([0-9.]+)/.test(String(k))) return undefined
-    const key = k.toString() as keyof typeof directionObj
-    if (!target[key]) {
-      const strK = String(key)
-      const size = strK.split(/(all|x|y|t|b|l|r)/)
-      const num = Number(size[2])
-      const tempList = []
-      switch (size[1]) {
-        case 'all':
-          tempList.push(direction[`x${num}`])
-          tempList.push(direction[`y${num}`])
-          break
-        case 'x':
-          tempList.push(direction[`l${num}`])
-          tempList.push(direction[`r${num}`])
-          break
-        case 'y':
-          tempList.push(direction[`t${num}`])
-          tempList.push(direction[`b${num}`])
-          break
-        case 'l':
-          target[`l${num}`] = css({ left: num && `${num}rem` })
-          break
-        case 'r':
-          target[`r${num}`] = css({ right: num && `${num}rem` })
-          break
-        case 't':
-          target[`t${num}`] = css({ top: num && `${num}rem` })
-          break
-        case 'b':
-          target[`b${num}`] = css({ bottom: num && `${num}rem` })
-          break
-      }
-      if (tempList.length) {
-        target[key] = tempList.flat()
-      }
-    }
-    return target[key]
-  }
-})
+const paddingObject: DirectionObjectType = {}
+export const padding = generateDirectionObject(
+  paddingObject,
+  'paddingLeft',
+  'paddingRight',
+  'paddingTop',
+  'paddingBottom'
+)

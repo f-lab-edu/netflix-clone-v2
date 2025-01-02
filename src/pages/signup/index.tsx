9b1cff1d2cba0cda64022127e951409c1ddff173
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-01-02 13:33:36 +0900

    rename: signup page

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,7 +1,7 @@
 import type { GetStaticProps } from 'next';
 import { getServerTranslations } from '@/lib/i18n/getServerTranslations'
 
-export { default } from '@/components/pages/Signup'
+export { default } from '@/components/pages/Signup/signup'
 
 export const getStaticProps: GetStaticProps = async ({
   locale,

d4fc0cb744594e6b2ff91ddf02ee63715b472f2a
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-01-02 13:34:12 +0900

    add: signup platform page

---
+++ src/components/pages/Signup/styles/SignupPlayform.ts
@@ -0,0 +1,17 @@
+import { css } from '@emotion/react';
+
+export const SignupPlatformContentCss = css({
+  width: '100%',
+  maxWidth: '560px',
+  margin: '0px auto',
+  padding: '20px 25px',
+  boxSizing: 'border-box',
+  transition: 'none',
+})
+
+export const SignupPlatformCardListCss = css({
+  display: 'flex',
+  alignItems: 'stretch',
+  flexDirection: 'row',
+  gap: '8px'
+})

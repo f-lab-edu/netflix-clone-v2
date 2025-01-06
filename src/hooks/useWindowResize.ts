4268c74f2d514df06d66552a8295da4b7417201a
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-01-07 01:17:52 +0900

    fix: window resize dependency

--- src/hooks/useWindowResize.ts
+++ src/hooks/useWindowResize.ts
@@ -12,6 +12,6 @@ export default function useWindowResize() {
     return () => {
       window.removeEventListener('resize', event)
     }
-  }, [])
+  }, [event])
   return [displayWidthDeferred]
 }

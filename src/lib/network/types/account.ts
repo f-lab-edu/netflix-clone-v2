9043ef73530c0c07f3d3f49dfc5444c487a14062
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-01-16 00:02:31 +0900

    add: accountSignup mock api

--- src/lib/network/types/account.ts
+++ src/lib/network/types/account.ts
@@ -1,9 +1,21 @@
+import type { ErrorResponse } from './error';
+
 export type EmailCheckRequestType = {
   email: string
 }
 export type EmailCheckResponseType = {
   checkResult: boolean
+} & ErrorResponse
+
+export type SignupRequestType = {
+  email: string
+  password: string
+  policy: boolean
+  specialOffer: boolean
 }
+export type SignupResponseType = {
+  result: boolean
+} & ErrorResponse
 
 export type RefreshTokenRequestType = {
   refreshToken: string
@@ -11,12 +23,14 @@ export type RefreshTokenRequestType = {
 export type RefreshTokenResponseType = {
   accessToken: string
   refreshToken: string
-}
+} & ErrorResponse
 
 export type LoginRequestType = {
   email: string
   password: string
 }
-export type LoginResponseType = RefreshTokenResponseType
+export type LoginResponseType = RefreshTokenResponseType & ErrorResponse
 
-export type MyInfoResponseType = AccountBaseInfo
+export type MyInfoResponseType = {
+  accountInfo: AccountBaseInfo
+} & ErrorResponse
--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,7 +1,18 @@
+import { HttpResponse } from 'msw'
 import AccountHandlers from './account'
 
 const handlers = [
   ...AccountHandlers
 ]
+
+const successResponseObj = {
+  errorCode: 0,
+  message: ''
+}
+
+export const createSuccessResponse = <T extends object>(responseData: T) => {
+  const obj = Object.assign(responseData, successResponseObj)
+  return HttpResponse.json<T & typeof successResponseObj>(obj, { status: 200, statusText: 'OK' })
+}
 
 export default handlers

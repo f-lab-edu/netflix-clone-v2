6aead9d997853520277093a9255d3d1a90a0f5aa
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-01-16 19:16:38 +0900

    fix: query as mutation

--- src/components/pages/Home/component/EmailSubmitForm.tsx
+++ src/components/pages/Home/component/EmailSubmitForm.tsx
@@ -1,8 +1,6 @@
-import type { EmailCheckResponseType } from '@/lib/network/types/account';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
-import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import DarkTextInput from '@/components/ui/Form/DarkTextInput';
 import { EmailCheck } from '@/lib/network/account/EmailCheck';
@@ -17,36 +15,29 @@ interface FormData {
 export default function EmailSubmitForm() {
   const { t } = useTranslation(['common', 'page-home'])
   const router = useRouter()
-  const { handleSubmit, register, formState, getFieldState } = useForm<FormData>({
+  const { handleSubmit, register, getFieldState } = useForm<FormData>({
     mode: 'onBlur',
     reValidateMode: 'onBlur',
     shouldUseNativeValidation: false,
   })
 
-  const { invalid, error, isTouched } = getFieldState('email', formState)
-  const [email, setEmail] = useState('')
-  const { refetch, isLoading, data } = useQuery({
-    queryKey: ['emailCheck', email],
-    enabled: false,
-    queryFn: async ({ queryKey }): Promise<EmailCheckResponseType> => {
-      const result = await EmailCheck(queryKey[1])
-      return result
+  const { invalid, error, isTouched } = getFieldState('email')
+  const { mutate, isPending } = useMutation({
+    mutationFn: async (email: string) => {
+      return await EmailCheck(email)
+    },
+    onSuccess(data) {
+      if (data.checkResult) {
+        router.push('/signin')
+      } else {
+        router.push('/signup')
+      }
     }
   })
   async function submitAction(obj: FormData) {
     sessionStorage.setItem('sign-tryed-email', obj.email)
-    setEmail(obj.email)
-    refetch()
-
+    mutate(obj.email)
   }
-  useEffect(() => {
-    if (!data) return
-    if (data.checkResult) {
-      router.push('/signin')
-    } else {
-      router.push('/signup')
-    }
-  }, [data, router])
   return <form onSubmit={handleSubmit(submitAction)}>
     <HeroDescrpition2>
       {t('page-home:section1.desc2')}
@@ -66,7 +57,7 @@ export default function EmailSubmitForm() {
       ></DarkTextInput>
       <button type="submit" css={EmailFormSubmitBtnCss}>
         {/* TODO: add spinner */}
-        {isLoading ? <div></div> : t('page-home:emailForm.button')}
+        {isPending ? <div></div> : t('page-home:emailForm.button')}
       </button>
     </div>
   </form>

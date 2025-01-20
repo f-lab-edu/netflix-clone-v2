902ee6518b65575b7ebc3d4e89ef6384f31e845a
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-01-20 21:57:17 +0900

    add: add payment page lang pack

--- types/i18n/index.d.ts
+++ types/i18n/index.d.ts
@@ -9,6 +9,7 @@ import 'i18next'
 // resources.ts file is generated with `npm run toc`
 import type common from '../../public/locales/en/common.json'
 import type PageHome from '../../public/locales/en/page-home.json'
+import type PagePayment from '../../public/locales/en/page-payment.json'
 import type PageSignup from '../../public/locales/en/page-signup.json'
 
 declare module 'i18next' {
@@ -17,6 +18,7 @@ declare module 'i18next' {
     resources: {
       common: typeof common,
       'page-home': typeof PageHome
+      'page-payment': typeof PagePayment
       'page-signup': typeof PageSignup
     }
   }

fd5294fcf7e4be331c8e18d8912c93a6753e2515
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-01-21 13:49:18 +0900

    fix: client only storage

--- src/state/util/Storage.ts
+++ src/state/util/Storage.ts
@@ -1,33 +1,23 @@
 import type { SyncStringStorage } from 'jotai/vanilla/utils/atomWithStorage';
 import { createJSONStorage } from 'jotai/utils';
 
-const isBrowser = typeof window !== 'undefined'
-const serverObj: { [k: string]: string } = {}
+const isBrowser = typeof window === 'undefined'
 
 export const CoveredLocalStorage: SyncStringStorage = {
   getItem: (key) => {
-    if (isBrowser) {
-      return localStorage.getItem(key)
-    } else {
-      return serverObj[key]
-    }
+    if (isBrowser) return ''
+    return localStorage.getItem(key)
   },
   setItem(key, newValue) {
-    if (isBrowser) {
-      localStorage.setItem(key, newValue)
-    } else {
-      serverObj[key] = newValue
-    }
+    if (isBrowser) return
+    localStorage.setItem(key, newValue)
   },
   removeItem(key) {
-    if (isBrowser) {
-      return localStorage.removeItem(key)
-    } else {
-      return serverObj[key]
-    }
+    if (isBrowser) return
+    localStorage.removeItem(key)
   },
   subscribe(key, callback) {
-    if (!isBrowser) return () => { }
+    if (isBrowser) return () => { }
     const windowEventListener = (e: StorageEvent) => {
       if (e.storageArea === localStorage && e.key === key) {
         callback(localStorage.getItem(key))

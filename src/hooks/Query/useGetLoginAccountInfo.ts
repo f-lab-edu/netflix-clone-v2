fd9cb0b83afd60bd3331c303234481b0874d58e3
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-01-24 12:00:20 +0900

    add: my info api

---
+++ src/hooks/Query/useGetLoginAccountInfo.ts
@@ -0,0 +1,17 @@
+import type { MyInfoResponseType } from '@/lib/network/types/account';
+import { useQuery } from '@tanstack/react-query';
+import { GetAccountInfoApi } from '@/lib/network/account/GetAccountInfoApi';
+import { MY_INFO_QUERY_KEY } from './keys/account';
+
+export default function useGetSigninAccountInfo(options: {
+  enabled: boolean
+}) {
+  return useQuery<MyInfoResponseType>({
+    queryKey: MY_INFO_QUERY_KEY,
+    queryFn: async () => {
+      const result = await GetAccountInfoApi()
+      return result
+    },
+    ...options
+  })
+}

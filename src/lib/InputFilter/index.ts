4286430d6c702a821483fbb85dc80670ae441e2c
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-01-26 19:35:19 +0900

    fix: number filter as \d

--- src/lib/InputFilter/index.ts
+++ src/lib/InputFilter/index.ts
@@ -1,22 +1,22 @@
 import type { ChangeEventHandler } from 'react';
 
 export const NumberInputFieldOnChangeEvent: ChangeEventHandler<HTMLInputElement> = (e) => {
-  e.target.value = e.target.value.replace(/[^0-9]+/g, '')
+  e.target.value = e.target.value.replace(/\D+/g, '')
 }
 
 export const CardNumberInputFieldOnChangeEvent: ChangeEventHandler<HTMLInputElement> = (e) => {
   NumberInputFieldOnChangeEvent(e)
-  e.target.value = e.target.value.split(/([0-9]{1,4})/).filter(Boolean).join(' ')
+  e.target.value = e.target.value.split(/(\d{1,4})/).filter(Boolean).join(' ')
 }
 
 export const CardExpireDateInputFieldOnChangeEvent: ChangeEventHandler<HTMLInputElement> = (e) => {
   NumberInputFieldOnChangeEvent(e)
-  e.target.value = e.target.value.split(/([0-9]{1,2})/).filter(Boolean).splice(0, 2).join('/')
+  e.target.value = e.target.value.split(/(\d{1,2})/).filter(Boolean).splice(0, 2).join('/')
 }
 
 export const BirthDateInputFieldOnChangeEventForEn: ChangeEventHandler<HTMLInputElement> = (e) => {
   NumberInputFieldOnChangeEvent(e)
-  const matchResult = /([0-9]{2})([0-9]{2})?([0-9]{4})?(.+)/.exec(e.target.value)
+  const matchResult = /(\d{2})(\d{2})?(\d{4})?(.+)/.exec(e.target.value)
   if (matchResult) {
     e.target.value = [matchResult[1], matchResult[2], matchResult[3], matchResult[4]].filter(Boolean).slice(0, 3).join('/')
   }
@@ -24,7 +24,7 @@ export const BirthDateInputFieldOnChangeEventForEn: ChangeEventHandler<HTMLInput
 
 export const BirthDateInputFieldOnChangeEventForKr: ChangeEventHandler<HTMLInputElement> = (e) => {
   NumberInputFieldOnChangeEvent(e)
-  const matchResult = /([0-9]{4})([0-9]{2})?([0-9]{2})?(.+)/.exec(e.target.value)
+  const matchResult = /(\d{4})(\d{2})?(\d{2})?(.+)/.exec(e.target.value)
   if (matchResult) {
     e.target.value = [matchResult[1], matchResult[2], matchResult[3], matchResult[4]].filter(Boolean).slice(0, 3).join('/')
   }

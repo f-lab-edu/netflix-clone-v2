ee7d6de93ff85f97c73fe99dd8b86b3f8cc5714e
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-01-27 22:38:40 +0900

    fix: event filter as text type filter

--- src/lib/InputFilter/index.ts
+++ src/lib/InputFilter/index.ts
@@ -1,31 +1,23 @@
-import type { ChangeEventHandler } from 'react';
-
-export const NumberInputFieldOnChangeEvent: ChangeEventHandler<HTMLInputElement> = (e) => {
-  e.target.value = e.target.value.replace(/\D+/g, '')
+export const NumberTextFilter = (t: string): string => {
+  return t.replace(/\D+/g, '')
 }
 
-export const CardNumberInputFieldOnChangeEvent: ChangeEventHandler<HTMLInputElement> = (e) => {
-  NumberInputFieldOnChangeEvent(e)
-  e.target.value = e.target.value.split(/(\d{1,4})/).filter(Boolean).join(' ')
+export const CardNumberTextFilter = (t: string): string => {
+  return NumberTextFilter(t).split(/(\d{1,4})/).filter(Boolean).join(' ')
 }
 
-export const CardExpireDateInputFieldOnChangeEvent: ChangeEventHandler<HTMLInputElement> = (e) => {
-  NumberInputFieldOnChangeEvent(e)
-  e.target.value = e.target.value.split(/(\d{1,2})/).filter(Boolean).splice(0, 2).join('/')
+export const ExpireDateTextFilter = (t: string): string => {
+  return NumberTextFilter(t).split(/(\d{1,2})/).filter(Boolean).splice(0, 2).join('/')
 }
 
-export const BirthDateInputFieldOnChangeEventForEn: ChangeEventHandler<HTMLInputElement> = (e) => {
-  NumberInputFieldOnChangeEvent(e)
-  const matchResult = /(\d{2})(\d{2})?(\d{4})?(.+)/.exec(e.target.value)
-  if (matchResult) {
-    e.target.value = [matchResult[1], matchResult[2], matchResult[3], matchResult[4]].filter(Boolean).slice(0, 3).join('/')
-  }
+export const BirthDateEnTypeFilter = (t: string): string => {
+  const numberText = NumberTextFilter(t)
+  const matchResult = /(\d{2})(\d{2})?(\d{4})?(.+)/.exec(numberText)
+  return matchResult ? [matchResult[1], matchResult[2], matchResult[3], matchResult[4]].filter(Boolean).slice(0, 3).join('/') : ''
 }
 
-export const BirthDateInputFieldOnChangeEventForKr: ChangeEventHandler<HTMLInputElement> = (e) => {
-  NumberInputFieldOnChangeEvent(e)
-  const matchResult = /(\d{4})(\d{2})?(\d{2})?(.+)/.exec(e.target.value)
-  if (matchResult) {
-    e.target.value = [matchResult[1], matchResult[2], matchResult[3], matchResult[4]].filter(Boolean).slice(0, 3).join('/')
-  }
+export const BirthDateKrTypeFilter = (t: string): string => {
+  const numberText = NumberTextFilter(t)
+  const matchResult = /(\d{4})(\d{2})?(\d{2})?(.+)/.exec(numberText)
+  return matchResult ? [matchResult[1], matchResult[2], matchResult[3], matchResult[4]].filter(Boolean).slice(0, 3).join('/') : ''
 }

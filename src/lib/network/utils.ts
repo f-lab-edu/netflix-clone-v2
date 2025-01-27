cd75d80d193dc15b154c4a7d0a9f1482d98f135b
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-01-27 17:46:16 +0900

    add: signup-membership state

--- src/lib/network/utils.ts
+++ src/lib/network/utils.ts
@@ -4,3 +4,5 @@ export const REFRESH_TOKEN_KEY = 'refresh-token'
 export const SIGNIN_EMAIL_OR_PHONE = 'signin-emailOrPhone'
 
 export const SIGNIN_CURRENT_PROFILE = 'profile-current-position'
+
+export const SIGNUP_MEMBERSHIP_TIER = 'signup-membership-tier'

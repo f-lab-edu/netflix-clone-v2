e11cc4eacff7ae8cb034aca71f8c3f921c1aadb9
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-02-04 20:32:04 +0900

    add: client only component

--- src/components/pages/Signup/platform.tsx
+++ src/components/pages/Signup/platform.tsx
@@ -2,8 +2,9 @@ import type { NextPageWithLayout } from '@/pages/_app';
 import { useAtom } from 'jotai';
 import Link from 'next/link';
 import { useTranslation } from 'next-i18next';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import SignupLayout from '@/components/layout/SignupLayout';
+import ClientOnly from '@/components/ui/utils/ClientOnly';
 import ConditionalRender from '@/components/ui/utils/ConditionalRender';
 import useJWTs from '@/hooks/account/useJWTs';
 import useWindowResize from '@/hooks/useWindowResize';
@@ -16,7 +17,6 @@ import { SignupPlatformContentCss, SignupPlatformContentLargeCss } from './style
 
 const PlatformPage: NextPageWithLayout = () => {
   const { t } = useTranslation(['page-signup'])
-
   const { hasLogin } = useJWTs()
   const [, setMembershipTier] = useAtom(signupMembershipTier)
   // on resize display width > over 1050px change contents as full width mode
@@ -25,29 +25,21 @@ const PlatformPage: NextPageWithLayout = () => {
   } = useWindowResize()
   const [selectedType, setSelectedType] = useState<MembershipPlanTier>('premium')
 
-  const [isClient, setIsClient] = useState(false)
-  useEffect(() => {
-    setIsClient(true)
-  }, [])
-
   const goNextAction = () => {
     setMembershipTier(selectedType)
   }
   return <>
     <div css={[SignupPlatformContentCss, isLarge ? SignupPlatformContentLargeCss : {}]}>
       <StepHeader css={{ marginBottom: '.5rem' }} step={2} title={t('page-signup:platform.title')} />
-      <ConditionalRender.Boolean
-        condition={isClient}
-        render={{
-          true: <ConditionalRender.Boolean
-            condition={isLarge}
-            render={{
-              true: <PlatformDetailLarge onSelectedChange={setSelectedType} selectedType={selectedType} />,
-              false: <PlatformDetailSlim onSelectedChange={setSelectedType} selectedType={selectedType} />
-            }}
-          />
-        }}
-      />
+      <ClientOnly>
+        <ConditionalRender.Boolean
+          condition={isLarge}
+          render={{
+            true: <PlatformDetailLarge onSelectedChange={setSelectedType} selectedType={selectedType} />,
+            false: <PlatformDetailSlim onSelectedChange={setSelectedType} selectedType={selectedType} />
+          }}
+        />
+      </ClientOnly>
     </div>
     <div css={[SignupMainContentCss, { marginTop: '24px' }]}>
       <Link css={SignupMainNextButtonCss} href={hasLogin ? '/signup/payment/regist/' : '/signup/registration'} onClick={goNextAction}>

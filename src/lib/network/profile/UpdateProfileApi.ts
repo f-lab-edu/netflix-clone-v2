7f0011f272c2d6391542f96843fd62d83f12cd47
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-02-06 23:34:59 +0900

    add: profile apis

---
+++ src/lib/network/profile/UpdateProfileApi.ts
@@ -0,0 +1,9 @@
+import type { UpdateProfileRequestType, UpdateProfileResponseType } from '../types/profile';
+import api from '..';
+
+export default async function UpdateProfileApi(profile: UpdateProfileRequestType) {
+  const result = await api.post<UpdateProfileResponseType>('profile', {
+    json: profile
+  })
+  return result
+}

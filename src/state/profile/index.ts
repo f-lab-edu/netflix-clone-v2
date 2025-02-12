f05cae6a598dcfc07454043a33c4ef661f6e8334
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-02-13 01:12:44 +0900

    fix: accountInfo types

--- src/state/profile/index.ts
+++ src/state/profile/index.ts
@@ -3,4 +3,4 @@ import { SIGNIN_CURRENT_PROFILE } from '@/lib/network/utils';
 import { JotaiSessionStorage } from '../util/Storage';
 
 // TODO: with sessionStorage
-export const currentProfileAtom = atomWithStorage(SIGNIN_CURRENT_PROFILE, undefined, JotaiSessionStorage<Profile | undefined>())
+export const currentProfileAtom = atomWithStorage(SIGNIN_CURRENT_PROFILE, undefined, JotaiSessionStorage<number | undefined>())

d39282986a570ae5bab1b53f78554bf899611a7f
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-02-13 18:27:21 +0900

    delete: legacy

--- src/components/ui/Form/LightTextInput/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { TextInputProps } from '../TextInput';
-import TextInput from '../TextInput';
-
-const LegacyLightTextInput = ({ placeholder, ...props }: Omit<TextInputProps, 'theme'>) => <TextInput.Light {...props} label={placeholder} />
-/**
- * @deprecated use TextInput.light component
- */
-export default LegacyLightTextInput

d63007107019e59ddbee8fb708891c0b15c7d771
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-02-26 01:45:44 +0900

    add: debounce option

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,19 +1,27 @@
 import { useCallback, useRef, useState } from 'react';
 
-export function useDebounceState<T>(initValue: T, delay: number): [T, (_input: T) => void] {
+export function useDebounceState<T>(initValue: T, delay: number, options?: {
+  clearAll?: boolean
+}): [T, (_input: T) => void] {
   const [debouncedValue, setDebouncedValue] = useState<T>(initValue);
   const value = useRef<T>(initValue)
   const timeout = useRef<NodeJS.Timeout | undefined>(undefined)
 
   const setState = useCallback((input: T) => {
     value.current = input
-    if (!timeout.current) {
-      timeout.current = setTimeout(() => {
-        setDebouncedValue(value.current)
-        timeout.current = undefined
-      }, delay)
+
+    if (options?.clearAll) {
+      clearTimeout(timeout.current)
+      timeout.current = undefined
     }
-  }, [delay])
+    if (timeout.current) return
+
+    timeout.current = setTimeout(() => {
+      setDebouncedValue(value.current)
+      timeout.current = undefined
+    }, delay)
+
+  }, [delay, options])
 
   return [debouncedValue, setState];
 }

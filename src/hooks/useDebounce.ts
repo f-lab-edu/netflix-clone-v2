9682e5351e0318a7cad64c63a6c016e1bcf2e8cf
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-02-27 21:00:14 +0900

    fix: debounce types

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -17,7 +17,7 @@ export function useDebounceState<T>(initValue: T, delay: number): [T, (_input: T
   return [debouncedValue, setState];
 }
 
-export function useDebounce<T extends (..._args: unknown[]) => void>(action: T, delay: number): T {
+export function useDebounce<T extends (..._args: any[]) => void>(action: T, delay: number): T {
   const before = useRef<NodeJS.Timeout | undefined>(undefined)
   const debounce = useCallback((...args: Parameters<T>) => {
     clearTimeout(before.current)

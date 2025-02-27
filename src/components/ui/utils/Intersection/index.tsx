4749852ce3bd589b2a88d86b2cea796be3aff610
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-02-27 15:47:19 +0900

    fix: intersection bug

--- src/components/ui/utils/Intersection/index.tsx
+++ src/components/ui/utils/Intersection/index.tsx
@@ -1,6 +1,6 @@
 import type { IntersectionCallback } from './lib';
 import type { ReactElement, RefCallback } from 'react';
-import { cloneElement } from 'react';
+import { cloneElement, useCallback, useRef } from 'react';
 import useIntersection from './hook';
 
 interface IntersectionProps {
@@ -14,13 +14,21 @@ export default function Intersection({
   onVisible,
   thresholds
 }: IntersectionProps) {
-  const callback: IntersectionCallback = (v) => {
+  const intersectionRatio = useRef<number>(0)
+  const callback: IntersectionCallback = useCallback((v) => {
+    if (intersectionRatio.current === v.intersectionRatio) return
+    intersectionRatio.current = v.intersectionRatio
+
     if (typeof thresholds === 'number') {
-      if (v.intersectionRatio >= thresholds) onVisible(v)
+      if (
+        intersectionRatio.current >= thresholds
+      ) {
+        onVisible(v)
+      }
     } else {
       onVisible(v)
     }
-  }
+  }, [onVisible, thresholds])
   const { ref } = useIntersection(callback)
   return cloneElement(children, { ref: ref })
 }

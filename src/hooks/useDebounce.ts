4638c62327416588fe35d40a507f9a3b1fb513db
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-03-10 14:46:08 +0900

    fix: debounse state types

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,6 +1,7 @@
+import type { Dispatch, SetStateAction } from 'react';
 import { useCallback, useRef, useState } from 'react';
 
-export function useDebounceState<T>(initValue: T, delay: number): [T, (_input: T) => void] {
+export function useDebounceState<T>(initValue: T, delay: number): [T, Dispatch<SetStateAction<T>>] {
   const [debouncedValue, setDebouncedValue] = useState<T>(initValue);
   const setState = useDebounce(setDebouncedValue, delay)
   return [debouncedValue, setState];

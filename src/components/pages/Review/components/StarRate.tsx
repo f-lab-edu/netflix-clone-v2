52e7debb0ad29d079c1a87db3df181f28e23c4eb
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-03-13 23:36:12 +0900

    add: star rate input

---
+++ src/components/pages/Review/components/StarRate.tsx
@@ -0,0 +1,23 @@
+import ConditionalRender from '@/components/utils/ConditionalRender'
+import StarEmptyIcon from '@assets/netflix/icon/starEmptyIcon.svg'
+import StarFillIcon from '@assets/netflix/icon/starFillIcon.svg'
+
+interface StarRateProps {
+  value: number
+  onChange: (_v: number) => void
+}
+
+export default function StarRate({ value, onChange }: StarRateProps) {
+  return <div css={{
+    display: 'flex'
+  }}>
+    {Array(5).fill(0).map((v, i) => <ConditionalRender.Boolean
+      key={`star-${i}`}
+      condition={value >= i + 1}
+      render={{
+        true: <StarFillIcon onClick={() => onChange(i + 1)} />,
+        false: <StarEmptyIcon onClick={() => onChange(i + 1)} />
+      }}
+    />)}
+  </div>
+}

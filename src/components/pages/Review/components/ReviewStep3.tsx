768c60a3b6705e378a2a4b20debabf1970387f94
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-03-14 00:37:13 +0900

    fix: re regist when step change

--- src/components/pages/Review/components/ReviewStep3.tsx
+++ src/components/pages/Review/components/ReviewStep3.tsx
@@ -1,5 +1,7 @@
 import type { UseFormReturn } from 'react-hook-form';
+import { useEffect } from 'react';
 import TextInput from '@/components/ui/Form/TextInput';
+import RHFValidErrorHelper from '@/components/ui/Form/utils/RHFValidErrorHelper';
 
 interface ReviewStepsProps extends Omit<UseFormReturn<DramaReviewFormData>, 'handleSubmit'> {
   onGoBackAction: () => void
@@ -7,15 +9,41 @@ interface ReviewStepsProps extends Omit<UseFormReturn<DramaReviewFormData>, 'han
 
 export default function ReviewStep3({
   onGoBackAction,
-  register
+  register,
+  unregister,
+  getValues,
+  formState,
 }: ReviewStepsProps) {
+  const rate = getValues('rate')
+  const commentOptions = rate === 1 || rate === 5 ? {
+    maxLength: {
+      value: 300,
+      message: '후기는 최소 100자에서 300자를 작성 하셔야 합니다.'
+    },
+    minLength: {
+      value: 100,
+      message: '후기는 최소 100자에서 300자를 작성 하셔야 합니다.'
+    }
+  } : undefined
+  useEffect(() => {
+    return () => {
+      unregister('comment', {
+        keepValue: true,
+      })
+    }
+  }, [])
   return <div>
     <div>
       <TextInput.Dark
         label="후기"
         {...register('comment', {
-          required: true,
+          required: Boolean(commentOptions),
+          ...commentOptions
         })}
+        {...RHFValidErrorHelper(
+          formState.errors.comment?.message,
+          formState.touchedFields.comment
+        )}
       />
     </div>
     <button onClick={onGoBackAction}>Goto Before</button>

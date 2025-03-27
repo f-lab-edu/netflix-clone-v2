de7296581f47d6c013a0b5d4da065d3a63f8f311
cjhih456 <42570411+cjhih456@users.noreply.github.com>
2025-03-27 18:12:02 +0900

    fix: starRate controlled with base component

--- src/components/pages/Review/atoms/HookFormStarRate/HookFormStarRate.tsx
+++ src/components/pages/Review/atoms/HookFormStarRate/HookFormStarRate.tsx
@@ -1,24 +1,17 @@
 import type { HookFormStarRateProps } from './types';
 import { Controller, useFormContext } from 'react-hook-form';
-import ConditionalRender from '@/components/utils/ConditionalRender'
-import StarEmptyIcon from '@assets/netflix/icon/starEmptyIcon.svg'
-import StarFillIcon from '@assets/netflix/icon/starFillIcon.svg'
+import StarRate from './StarRate';
 
-export default function HookFormStarRate({ minValue = 1, maxValue = 5, name }: HookFormStarRateProps) {
+export default function HookFormStarRate({ name, ...starRateProps }: HookFormStarRateProps) {
   const { control } = useFormContext()
   return <Controller
     control={control}
     name={name}
-    render={({ field: { onChange, value } }) => <div css={{ display: 'flex' }}>
-      {Array(maxValue - (minValue - 1)).fill(0).map((v, i) => <ConditionalRender.Boolean
-        key={`star-${i}`}
-        condition={value >= i + minValue}
-        render={{
-          true: <StarFillIcon onClick={() => onChange(i + minValue)} />,
-          false: <StarEmptyIcon onClick={() => onChange(i + minValue)} />
-        }}
-      />)}
-    </div>}
+    render={({ field: { onChange, value } }) => <StarRate
+      {...starRateProps}
+      value={value}
+      onChange={onChange}
+    />}
   />
 
 }
--- src/components/pages/Review/atoms/HookFormStarRate/types.ts
+++ src/components/pages/Review/atoms/HookFormStarRate/types.ts
@@ -1,5 +1,13 @@
-export interface HookFormStarRateProps {
+export interface StarRateBaseProps {
   maxValue?: number
   minValue?: number
+}
+
+export interface StarRateProps extends StarRateBaseProps {
+  value: number
+  onChange: (_v: number) => void
+}
+
+export interface HookFormStarRateProps extends StarRateBaseProps {
   name: string
 } 
